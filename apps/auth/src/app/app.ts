f0b693ae2336a2e807108507eb8cff1cbc30ce33
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2024-06-13 19:30:31 +0300

    feat: add global error handler

--- apps/auth/src/app/app.ts
+++ apps/auth/src/app/app.ts
@@ -1,7 +1,17 @@
+/* eslint-disable functional/no-expression-statements */
+/* eslint-disable functional/functional-parameters */
 import { server, env } from '#/configs'
 import { auth } from '#/router'
+import { httpErrors } from '#/utils/constants'
+import { createResponseError } from '#/utils/helpers'
 
 function createApp() {
+  server.setErrorHandler(function (error, request, rep) {
+    const errorMessage = error.message
+    const responseError = createResponseError(httpErrors.badRequest, errorMessage)
+    return rep.status(responseError.code).send(responseError)
+  })
+
   server.register(auth)
 
   return server
@@ -17,6 +27,7 @@ async function startApp() {
     await app.listen({ port })
   } catch (error) {
     app.log.error(error)
+    // eslint-disable-next-line functional/no-throw-statements
     throw error
   }
 }
--- apps/auth/src/router/auth.route.ts
+++ apps/auth/src/router/auth.route.ts
@@ -2,14 +2,13 @@ import { FastifyInstance } from 'fastify'
 
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
-import { ZodError } from 'zod'
-
 import { createSession } from '#/controllers'
 import { createSessionBodySchema, createSessionResponseSchema, responseErrorSchema } from '#/schema'
-import { httpErrors, httpSuccess } from '#/utils/constants'
-import { createResponseError } from '#/utils/helpers'
+import { httpSuccess } from '#/utils/constants'
 
+// eslint-disable-next-line functional/prefer-immutable-types
 async function AuthRoute(fastify: FastifyInstance) {
+  // eslint-disable-next-line functional/no-expression-statements
   fastify.withTypeProvider<ZodTypeProvider>().route({
     method: 'POST',
     url: '/login',
@@ -22,16 +21,8 @@ async function AuthRoute(fastify: FastifyInstance) {
     },
     attachValidation: true,
     async handler(request, rep) {
-      if (request.validationError) {
-        const errorMessage =
-          request.validationError instanceof ZodError
-            ? request.validationError.issues[0].message
-            : 'An unknown validation error occured'
-        const error = createResponseError(httpErrors.badRequest, errorMessage)
-        rep.status(error.code).send(error)
-        return
-      }
       const token = await createSession(request.body)
+      // eslint-disable-next-line functional/no-expression-statements
       rep.status(httpSuccess.created).send(token)
     }
   })
